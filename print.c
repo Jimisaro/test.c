9fbe9cf140dfbe618ffe83ca0904029e77849ed3
jimmymulei <jimmymulei250@gmail.com>
2026-04-14 01:09:14 +0300

    initial commit

---
+++ print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,total;
+    printf("enter the num1 below");
+    scanf("%d",&num1);
+    printf("enter num2 below");
+    scanf("%d",&num2);
+    total=num1+num2;
+    printf("total=%d\n",total);
+    return 0;
+    
+}
